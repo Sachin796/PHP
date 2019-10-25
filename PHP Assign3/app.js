0820baa120f667cf0a3f13074b083915d765dacc
Sachin796 <sachinjaveri.2010@gmail.com>
2019-10-24 23:16:49 -0400

    Responsive and sidenav feature

--- PHP Assign3/app.js
+++ PHP Assign3/app.js
@@ -1,20 +1,34 @@
+this.className = "";
+//  document.location.refresh();
 var burger = document.querySelector(".burger");
 var nav = document.querySelector(".nav-links");
-var container=document.getElementsByClassName("container")[0];
+var btnslide = document.querySelector(".butn");
+var iconNav = document.querySelector(".icon");
+var iconNavLink = document.querySelector(".nav-link");
+
+var container = document.getElementsByClassName("container")[0];
 const navSlide = () => {
+  // Burger menu click
   burger.addEventListener("click", () => {
-    document.getElementsByClassName("nav-links")[0].style.transition="0.5s ease-out";
+    document.getElementsByClassName("nav-links")[0].style.transition =
+      "0.5s ease-out";
 
     nav.classList.toggle("nav-active");
     burger.classList.toggle("toggle");
     container.classList.toggle("container-left");
-    console.log(nav.classList.value);
-    console.log(burger.classList.value);
   });
-  //   nav.classList.toggle(".nav-links");
-  //     burger.classList.toggle(".burger");
-  console.log(nav.classList.value);
-  console.log(burger.classList.value);
+
+  // Icon click
+  iconNav.addEventListener("click", () => {
+    //  document.getElementsByClassName("nav-link")[0].style.transition="0.5s ease-out";
+    iconNavLink.classList.toggle("iconNav-active");
+    console.log(iconNav.classList.value);
+  });
+
+  // Side btn clicked
+  btnslide.addEventListener("click", () => {
+  
+  });
 };
 
 navSlide();
